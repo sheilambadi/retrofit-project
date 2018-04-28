675a4a85dbf1a44b7de57530ad54983d6e6699e2
sheilambadi <mbadiatieno@gmail.com>
2018-04-29 02:40:01 +0300

    Change return type from Class to Call

--- app/src/main/java/com/sheilambadi/android/retrofitproject/rest/ApiInterface.java
+++ app/src/main/java/com/sheilambadi/android/retrofitproject/rest/ApiInterface.java
@@ -2,6 +2,7 @@ package com.sheilambadi.android.retrofitproject.rest;
 
 import com.sheilambadi.android.retrofitproject.model.MovieResponse;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -9,9 +10,9 @@ import retrofit2.http.Query;
 public interface ApiInterface {
     //endpoint to top rated movies ~ first 20
     @GET("movie/top_rated")
-    Class<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
 
     //endpoint to a specific movie
     @GET("movie/{id}")
-    Class<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+    Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 }
