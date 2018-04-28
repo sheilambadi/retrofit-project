cd1b34f46815d3bc94b922635e8f130ac69ffecf
sheilambadi <mbadiatieno@gmail.com>
2018-04-29 01:48:50 +0300

    Create Retrofit instance as Http Client

---
+++ app/src/main/java/com/sheilambadi/android/retrofitproject/rest/ApiClient.java
@@ -0,0 +1,23 @@
+package com.sheilambadi.android.retrofitproject.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    //base URL
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    //create retrofit instance to send requests to API ~ singleton object
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(){
+        if (retrofit == null){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
