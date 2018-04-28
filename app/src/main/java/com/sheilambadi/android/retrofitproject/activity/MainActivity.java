c3a5bd0ef13b6d6041755084da4652a68866b95d
sheilambadi <mbadiatieno@gmail.com>
2018-04-29 02:41:12 +0300

    Implement getTopRatedMovies request

--- app/src/main/java/com/sheilambadi/android/retrofitproject/activity/MainActivity.java
+++ app/src/main/java/com/sheilambadi/android/retrofitproject/activity/MainActivity.java
@@ -1,15 +1,47 @@
 package com.sheilambadi.android.retrofitproject.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.sheilambadi.android.retrofitproject.R;
+import com.sheilambadi.android.retrofitproject.model.Movie;
+import com.sheilambadi.android.retrofitproject.model.MovieResponse;
+import com.sheilambadi.android.retrofitproject.rest.ApiClient;
+import com.sheilambadi.android.retrofitproject.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    //the movie db api key
+    private static final String API_KEY = "371550cc422e64f0e576183b8aac074e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //implement the interface methods to make HTTP call/request
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                Log.d(TAG, "Number of movies received " + movies.size());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
     }
 }
--- app/src/main/java/com/sheilambadi/android/retrofitproject/rest/ApiClient.java
+++ app/src/main/java/com/sheilambadi/android/retrofitproject/rest/ApiClient.java
@@ -10,9 +10,9 @@ public class ApiClient {
     //create retrofit instance to send requests to API ~ singleton object
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(){
-        if (retrofit == null){
-            Retrofit retrofit = new Retrofit.Builder()
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
