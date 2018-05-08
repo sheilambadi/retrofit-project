3ba5bf43a991f1047aa7586fd5c13b74b9c78ed2
sheilambadi <mbadisheila@gmail.com>
2018-05-08 13:10:00 +0300

    Add pagination adapter

---
+++ app/src/main/java/com/sheilambadi/android/retrofitproject/adapter/MoviesPaginationAdapter.java
@@ -0,0 +1,222 @@
+package com.sheilambadi.android.retrofitproject.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.sheilambadi.android.retrofitproject.GlideApp;
+import com.sheilambadi.android.retrofitproject.R;
+import com.sheilambadi.android.retrofitproject.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 5/8/2018.
+ */
+
+public class MoviesPaginationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int ITEM = 0;
+    private static final int LOADING = 1;
+    private static final String BASE_URL_IMG = "https://image.tmdb.org/t/p/w150";
+
+    private List<Movie> movieResults;
+    private Context context;
+
+    private boolean isLoadingAdded = false;
+
+    public MoviesPaginationAdapter(Context context) {
+        this.context = context;
+        movieResults = new ArrayList<>();
+    }
+
+    public List<Movie> getMovies() {
+        return movieResults;
+    }
+
+    public void setMovies(List<Movie> movieResults) {
+        this.movieResults = movieResults;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
+        switch (viewType) {
+            case ITEM:
+                viewHolder = getViewHolder(parent, inflater);
+                break;
+            case LOADING:
+                View v2 = inflater.inflate(R.layout.list_item_movie, parent, false);
+                viewHolder = new LoadingVH(v2);
+                break;
+        }
+        return viewHolder;
+    }
+
+    @NonNull
+    private RecyclerView.ViewHolder getViewHolder(ViewGroup parent, LayoutInflater inflater) {
+        RecyclerView.ViewHolder viewHolder;
+        View v1 = inflater.inflate(R.layout.list_item_movie, parent, false);
+        viewHolder = new MovieVH(v1);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Movie movie = movieResults.get(position);
+
+        switch (getItemViewType(position)){
+            case ITEM:
+                final MovieVH movieVH = (MovieVH) holder;
+
+                GlideApp.with(context)
+                        .load(BASE_URL_IMG + movie.getPosterPath())
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                Toast.makeText(context, "Failed to load image", Toast.LENGTH_SHORT).show();
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                // image ready, hide progress now
+                                movieVH.posterLoading.setVisibility(View.GONE);
+                                return false; // return false if you want Glide to handle everything else.
+                            }
+                        })
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)   // cache both original & resized image
+                        .centerCrop()
+                        .placeholder(new ColorDrawable(Color.GRAY))
+                        .into(movieVH.poster);
+                movieVH.movieTitle.setText(movie.getOriginalTitle());
+                // Todo: display genres
+                movieVH.movieGenres.setText(movie.getReleaseDate());
+                movieVH.movieRating.setText(movie.getVoteAverage().toString());
+                movieVH.movieDescription.setText(movie.getOverview());
+
+                break;
+            case LOADING:
+                // do nothing
+                break;
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieResults == null ? 0 : movieResults.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == movieResults.size() - 1 && isLoadingAdded) ? LOADING : ITEM;
+    }
+
+    // helpers
+
+    public void add(Movie r) {
+        movieResults.add(r);
+        notifyItemInserted(movieResults.size() - 1);
+    }
+
+    public void addAll(List<Movie> moveResults) {
+        for (Movie movie : moveResults) {
+            add(movie);
+        }
+    }
+
+    public void remove(Movie r) {
+        int position = movieResults.indexOf(r);
+        if (position > -1) {
+            movieResults.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public void clear() {
+        isLoadingAdded = false;
+        while (getItemCount() > 0) {
+            remove(getItem(0));
+        }
+    }
+
+    public boolean isEmpty() {
+        return getItemCount() == 0;
+    }
+
+
+    public void addLoadingFooter() {
+        isLoadingAdded = true;
+        add(new Movie());
+    }
+
+    public void removeLoadingFooter() {
+        isLoadingAdded = false;
+
+        int position = movieResults.size() - 1;
+        Movie movie = getItem(position);
+
+        if (movie != null) {
+            movieResults.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public Movie getItem(int position) {
+        return movieResults.get(position);
+    }
+
+
+
+    /**
+     * Main list's content ViewHolder
+     */
+    protected class MovieVH extends RecyclerView.ViewHolder {
+        private ImageView poster;
+        private ProgressBar posterLoading;
+        private TextView movieTitle;
+        private TextView movieRating;
+        private TextView movieGenres;
+        private TextView movieDescription;
+
+
+        public MovieVH(View itemView) {
+            super(itemView);
+
+            poster = itemView.findViewById(R.id.img_card);
+            posterLoading = itemView.findViewById(R.id.pb_movies_list_img);
+            movieTitle = itemView.findViewById(R.id.tv_movie_title);
+            movieRating = itemView.findViewById(R.id.tv_ratings);
+            movieGenres = itemView.findViewById(R.id.tv_movie_genres);
+            movieDescription = itemView.findViewById(R.id.tv_movie_description);
+        }
+    }
+
+    protected class LoadingVH extends RecyclerView.ViewHolder {
+
+        public LoadingVH(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
